71ef1dbeaf119557d86175ea129e29a318f4c72e
wjywy <2934610933@qq.com>
2023-05-29 13:00:52 +0800

    目录结构

--- 代码随想录/动态规划/10-目标和.ts
+++ 代码随想录/动态规划/10-目标和.ts
@@ -15,5 +15,19 @@
  * 关键：如何转换为01背包模型
  */
 
-// const toSum = (nums: number[], S: number): number => {};
-// toSum([1, 1, 1, 1, 1], 3);
+function findTargetSumWays(nums: number[], target: number): number {
+  // 把数组分成两个组合left, right。left + right = sum, left - right = target.
+  const sum: number = nums.reduce((a: number, b: number): number => a + b);
+  if ((sum + target) % 2 || Math.abs(target) > sum) return 0;
+  const left: number = (sum + target) / 2;
+  const dp: number[] = new Array(left + 1).fill(0);
+  dp[0] = 1; // 装满容量为0的背包有1种方法（什么也不装）
+  for (let i: number = 0; i < nums.length; i++) {
+    for (let j: number = left; j >= nums[i]; j--) {
+      dp[j] += dp[j - nums[i]];
+    }
+  }
+  console.log(dp[left]);
+  return dp[left];
+}
+findTargetSumWays([1, 1, 1, 1, 1], 3);
--- 前端面试手撕题/手写ajax.ts
+++ 前端面试手撕题/手写ajax.ts
@@ -1,3 +1,14 @@
 /**
- *
+ * 首先建立一个xhr对象，然后开启异步模式
+ * send后自动关闭
+ * 需要用xhr.open()
  */
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "/login", true); /**开启异步 */
+xhr.onreadystatechange = function () {
+  if (xhr.readyState !== 4) return;
+  if (xhr.status === 200) {
+  } else {
+  }
+};
