0233f269746b32eee91d11865e3ee828205b2461
wjywy <2934610933@qq.com>
2023-06-01 11:06:51 +0800

    完整promise

--- 前端面试手撕题/promise相关/promise.js
+++ 前端面试手撕题/promise相关/promise.js
@@ -66,9 +66,9 @@ class Promise_ {
     onReject = typeof onReject === 'function' ? onReject : val => { throw (val) }
 
     /**实现then的链式调用，then可以执行链式调用的原因: then返回的是一个新的promise对象 */
-    let thenPromise = new Promise_((resolve, reject) => {
+    var thenPromise = new Promise_((resolve, reject) => {
       let resolvePromise = (resultFun) => {
-        /**微任务，用定时器实现 */
+        /**微任务，用定时器实现,使resolvePromise异步执行 */
         setTimeout(() => {
           try {
             let x = resultFun(this.promiseResult)
@@ -76,6 +76,7 @@ class Promise_ {
               throw new Error('不能返回自身')
             }
             if (x instanceof Promise_) {
+
               x.then(resolve, reject)
             } else {
               /**非Promise则直接成功 */
@@ -102,5 +103,117 @@ class Promise_ {
       }
     })
     return thenPromise
+  }
+
+  /**执行then的第二个参数 */
+  catch(onRejected) {
+    this.then(undefined, onRejected)
+  }
+
+  /**无论怎么都会执行 */
+  finally(onFinally) {
+    this.then((onFinally) => {
+      onFinally()
+    }, () => {
+      onFinally()
+    })
+  }
+
+  /** 
+   * 接收一个Promise数组，数组中若有非Promise项，则此项当作成功
+   * 如果所有Promise都成功，则返回成功结果数组
+   * 如果有一个Promise失败，则返回这个失败结果
+   * @param {Promise_[]} allPromise 
+  */
+  all(allPromise) {
+    let result = []
+    let count = 0
+    return new Promise_((resolve, reject) => {
+      const addData = (index, value) => {
+        result[index] = value
+        count++
+        if (count === allPromise.length) {
+          resolve(result)
+        }
+      }
+      allPromise.forEach((item, index) => {
+        if (item instanceof Promise_) {
+          item.then((res) => addData(index, res), (err) => reject(err))
+        } else {
+          addData(index, item)
+        }
+      })
+    })
+  }
+
+  /**
+   * 接收一个Promise数组，数组中若有非Promise项，则此项当作成功
+   * 如果有一个promise成功，则返回这个成功结果
+   * 如果有一个promise失败，则返回这个失败结果
+   * @param {Promise_[]} allPromise 
+  */
+  race(allPromise) {
+    return new Promise_((resolve, reject) => {
+      allPromise.forEach((promise, index) => {
+        if (promise instanceof Promise_) {
+          promise.then((res) => resolve(res), (err) => reject(err))
+        } else {
+          resolve(promise)
+        }
+      })
+    })
+  }
+
+  /**
+   * 接收一个promise数组，数组中若有非promise项，则把此项当作成功
+   * 把每一个promise的结果，集合成数组，返回
+   */
+  allSettled(allPromise) {
+    return new Promise_((resolve, reject) => {
+      let result = []
+      let count = 0
+      const addData = (value, item, index) => {
+        result[index] = { value, item }
+        count++
+        if (count === allPromise.length) {
+          resolve(result)
+        }
+      }
+      allPromise.forEach((item, index) => {
+        if (item instanceof Promise_) {
+          item.then((res) => addData('fulfilled', res, index), (error) => addData('rejected', error, index))
+        } else {
+          addData(item, index)
+        }
+      })
+    })
+  }
+
+  /**
+   * 接收一个Promise数组，数组中若有非promise项，则把此项当作成功
+   * 如果有一个Promise成功，则返回这个成功结果
+   * 如果所有Promise都失败，则返回这个失败结果数组
+   * @param {Promise_[]} allPromise 
+   */
+  any(allPromise) {
+    return new Promise_((resolve, reject) => {
+      let result = []
+      let count = 0
+      const addResult = (item, index) => {
+        result[index] = item
+        count++
+        if (count === allPromise.length) {
+          reject(result)
+        }
+      }
+
+      allPromise.forEach((promise, index) => {
+        if (promise instanceof Promise_) {
+          promise.then((res) => resolve(res), (err) => addResult(err, index))
+        } else {
+          resolve(promise)
+        }
+      })
+    })
   }
 }
